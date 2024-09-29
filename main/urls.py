af045bd65fdbe7be35550d7b5ed7e66bbf172ec4
Nikita Naumenko <solncevnikita80@gmail.com>
2024-09-29 19:41:27 +0300

    1. Connecting the application to the project.
    2. The application runs via the link /user/.
    
    3. The application has been registered in the project.

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from .settings import main
+
+from user import user_app, render_user
+
+user_app.add_url_rule(
+    rule = "/user/",
+    view_func = render_user,
+    methods = ["GET", "POST"]
+)
+
+main.register_blueprint(user_app)
