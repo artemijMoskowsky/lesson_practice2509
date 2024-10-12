6210b191acfe7a5fde6c3f23de1428d3d47216ba
Artem Moskowsky <artemij.mosckowsky.01062008@gmail.com>
2024-10-12 20:32:02 +0300

    Database was initialized

--- main/settings.py
+++ main/settings.py
@@ -1,7 +1,13 @@
 import flask
+import flask_migrate
+import flask_sqlalchemy
 
 main = flask.Flask(
     import_name = "main",
     static_url_path = "/main/",
     template_folder = "templates"
 )
+
+main.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+DATABASE = flask_sqlalchemy.SQLAlchemy(app = main)
+migrate = flask_migrate.Migrate(app = main, db = DATABASE)
