139380b755c014c14bc54da5bb5bc03d203d914a
Matvii Sivaiev <138612027+MatviiSivaiev2009@users.noreply.github.com>
2024-10-13 13:35:08 +0700

    Було створено форму для заповнення відгука, також зроблена відправка на пошту та форма листа

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 import home
 from .settings import main
 
-home.home.add_url_rule(rule = "/", view_func = home.render_home)
+home.home.add_url_rule(rule = "/", view_func = home.render_home, methods = ["POST", "GET"])
 main.register_blueprint(blueprint = home.home)
 
 
