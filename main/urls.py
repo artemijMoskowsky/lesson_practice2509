c180b2444567f340c4fd17395fe7642ab4d26ba7
Nikita Naumenko <solncevnikita80@gmail.com>
2024-10-14 17:53:02 +0300

    bugs in the code were fixed

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,24 @@
 from .settings import main
-from user import user_app, render_user
+from user import user_app, render_user, render_registration, render_login
 
 user_app.add_url_rule(
     rule = "/user/",
     view_func = render_user,
     methods = ["GET", "POST"]
 )
+
+user_app.add_url_rule(
+    rule = "/user/registration/",
+    view_func = render_registration,
+    methods = ["GET", "POST"]
+)
+
+user_app.add_url_rule(
+    rule = "/user/login/",
+    view_func = render_login,
+    methods = ["GET", "POST"]
+)
+
 
 
 main.register_blueprint(user_app)
--- user/views.py
+++ user/views.py
@@ -12,16 +12,15 @@ def render_user():
     return flask.render_template(template_name_or_list = "user.html")
 
 def render_registration():
-  
     confirm = False
     if flask.request.method == "POST":
         name = flask.request.form["username"]
         password = flask.request.form["password"]
         try:
-            is_user = False
             users = User.query.all()
+            is_user = False
             for user in users:
-                if user.name == name and user.password == password:
+                if user.username == name and user.password == password:
                     is_user = True  
             if not is_user:
                 confirm = True
@@ -33,7 +32,7 @@ def render_registration():
     return flask.render_template(template_name_or_list = "registration.html", confirm = confirm, link = "None")
 
 def render_login():
-    global users
+    
     confirm = True
     if flask.request.method == "POST":
         confirm = False
