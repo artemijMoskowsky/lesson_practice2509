83d874325f5a64c6cd55f96541f64a7ec37201d2
Julia Ovcharenko <samsungyuliyu@gmail.com>
2024-11-04 20:08:03 +0200

    test

--- tour_page/app.py
+++ tour_page/app.py
@@ -3,5 +3,7 @@ import flask
 tour_app = flask.Blueprint(
     name = 'tour',
     import_name = 'tour_page',
-    template_folder = 'templates'
+    template_folder = 'templates',
+    static_folder = 'static',
+    static_url_path = '/tour_page/'
 )
