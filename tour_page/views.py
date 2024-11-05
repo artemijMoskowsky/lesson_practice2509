a05a37c13136894db8cd56758aac76dcd0b5e0d5
Julia Ovcharenko <samsungyuliyu@gmail.com>
2024-11-05 22:42:15 +0200

    Initial design has been created. Will be edited

--- tour_page/views.py
+++ tour_page/views.py
@@ -7,18 +7,21 @@ def render_tour():
 
     # trip_to_turkey = Trip(title = 'Trip to Turkey', date = '13.10.2025', country = 'Turkey', price = '100$', description = 'abcd')
 
-    # trip_to_australia = Trip(
-    #     title = 'Trip to Australia', 
-    #     date = '23.10.2025', 
-    #     country = 'Australia', 
-    #     price = '200$', 
-    #     description = 'qwer')
+    # # trip_to_australia = Trip(
+    # #     title = 'Trip to Australia', 
+    # #     date = '23.10.2025', 
+    # #     country = 'Australia', 
+    # #     price = '200$', 
+    # #     description = 'qwer')
 
-    # DATABASE.session.add(trip_to_australia)
+    # trip_to_italy = Trip(title = 'l', date = '13.10.2025', country = 'Italy', price = '100$', description = 'abcd')
+
+
+    # DATABASE.session.add(trip_to_italy)
     # DATABASE.session.commit()
 
     
-    return flask.render_template(template_name_or_list = 'tour.html', trip_to_turkey2 = Trip.query.all())
+    return flask.render_template(template_name_or_list = 'tour.html', all_trips = Trip.query.all())
 
 def show_trip(id):
-    return flask.render_template(template_name_or_list = "tour_show.html", t_turkey = Trip.query.get(id))
+    return flask.render_template(template_name_or_list = "tour_show.html", countries = Trip.query.get(id))
